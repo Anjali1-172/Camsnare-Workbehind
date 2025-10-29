e6d07d37b3f2b98205a2545eca55577a793da368
Anjali1-172 <anjalipatlan722@gmail.com>
2025-10-29 20:51:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, render_template
 from flask_cors import CORS
 import Fusion_Engine
+import os 
 
 app = Flask(__name__)
 CORS(app)
@@ -34,5 +35,8 @@ def optical_scan():
         return jsonify({"status": "error", "message": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=8800, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
+
 
